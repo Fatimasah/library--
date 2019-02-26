6d7e0a25e9c614911006e8e02e30c9097099dc84
Fatima Sahmoud <fatimasahmouod@hotmail.com>
2019-02-26 12:26:07 +0200

    final project with modification

--- js/main.js
+++ js/main.js
@@ -9,9 +9,9 @@ $("document").ready(function ($) {
                 event.preventDefault();
                 var hash = this.hash;
                 $('html, body').animate({
-                    scrollTop: $(hash).offset().top - 140
+                    scrollTop: $(hash).offset().top - 132
                 }, 1000, function () {
-                    window.location.hash = hash - 140;
+                    window.location.hash = hash - 132;
                 });
             }
 
