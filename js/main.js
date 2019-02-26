97029efbb369b178f418cdb5b131ede006995061
Fatima Sahmoud <fatimasahmouod@hotmail.com>
2019-02-26 09:47:19 +0200

    final project

--- js/main.js
+++ js/main.js
@@ -1,7 +1,45 @@
 
-$("document").ready(function($){
+$("document").ready(function ($) {
 
-// navbar change background
+    // for navigation 
+    var anchor = $('.nav-link');
+    anchor.on('click', function (event) {
+        if ($(window).width() <= 767) {
+            if (this.hash !== "") {
+                event.preventDefault();
+                var hash = this.hash;
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top - 140
+                }, 1000, function () {
+                    window.location.hash = hash - 140;
+                });
+            }
+
+        } else if ($(window).width() >= 768 && $(window).width() <= 992) {
+            if (this.hash !== "") {
+                event.preventDefault();
+                hash = this.hash;
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top - 142
+                }, 1000, function () {
+                    window.location.hash = hash - 142;
+                });
+            }
+        } else if ($(window).width() > 991) {
+            if (this.hash !== "") {
+                event.preventDefault();
+                hash = this.hash;
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top - 83
+                }, 1000, function () {
+                    window.location.hash = hash - 83;
+                });
+            }
+        }
+    });
+    // end for navigation 
+
+    // navbar change background
     var nav = $('#mainnav');
     var logo = $('.logo');
     $(window).scroll(function () {
@@ -13,20 +51,21 @@ $("document").ready(function($){
             logo.removeClass("sticky-logo");
 
         }
+    });
+    // end
 
-        });
+    // contact
+    $(".whats-fixed").click(function () {
+        $(".fixed-contact").css("display", "block");
+    });
+    $(".close").click(function () {
+        $(".fixed-contact").css("display", "none");
+    });
 
-        $(".whats-fixed").click(function(){
-            $(".fixed-contact").css("display", "block");
-          });
-          $(".close").click(function(){
-            $(".fixed-contact").css("display", "none");
-          });
-        
- }); //end ready function
+}); //end ready function
 
 
 $('#myCarousel').carousel({
-    interval: 3000,
- })
+    interval: 4000,
+});
 
